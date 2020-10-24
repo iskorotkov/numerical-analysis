9e44e677e60d9bb753b079d016f69cd3e60ff2ba
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-24 13:58:13 +0500

    refactor(equations): update equations solver to use math expressions project

--- src/NonLinearEquations/Program.cs
+++ src/NonLinearEquations/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using MathExpressions;
+using MathExpressions.Terms;
 using SLAE.DirectMethods;
-using static System.Math;
 
 namespace NonLinearEquations
 {
@@ -8,10 +9,38 @@ namespace NonLinearEquations
     {
         private static void Main()
         {
-            var xk = new[]
+            var values = new[]
             {
-                new[] {0.4},
-                new[] {1.0}
+                new[] {-0.1},
+                new[] {0.5}
+            };
+
+            var x = T.Var(0);
+            var y = T.Var(1);
+
+            var f1 = -T.Value(1) + T.Sin(y + 0.5);
+            var f2 = -T.Cos(x - 2);
+
+            var f = new[]
+            {
+                new[] {f1},
+                new[] {f2}
+            };
+            var fGrad = new[]
+            {
+                new[] {f1.GradBy((Var) x), f1.GradBy((Var) y)},
+                new[] {f2.GradBy((Var) x), f2.GradBy((Var) y)}
+            };
+
+            f.SolveWithSimpleIterationMethod(fGrad, values.CreateCopy());
+
+            f.SolveWithNewtonMethod(fGrad, values.CreateCopy());
+
+            var minimizedF = f1 * f1 + f2 * f2;
+            var minimizedGrad = new[]
+            {
+                new[] {minimizedF.GradBy((Var) x)},
+                new[] {minimizedF.GradBy((Var) y)}
             };
 
             var parameters = new GradientDescendParameters
@@ -19,35 +48,9 @@ namespace NonLinearEquations
                 Alpha = 1.0,
                 Lambda = 0.5
             };
-
-            var f = new[]
-            {
-                new Fun2[] {(x, y) => -1 + Sin(y + 0.5)},
-                new Fun2[] {(x, y) => -Cos(x - 2)}
-            };
-
-            var fGrad = new[]
-            {
-                new Fun2[] {(_, _) => 0d, (_, y) => Cos(y + 0.5)},
-                new Fun2[] {(x, _) => Sin(x - 2), (_, _) => 0d}
-            };
-
-            Fun2 minimizedF = (x, y) => Pow(f[0][0](x, y), 2) + Pow(f[1][0](x, y), 2);
-            var minimizedGrad = new[]
-            {
-                new Fun2[] {(x, y) => -2 * Cos(x - 2) * Sin(x - 2)},
-                new Fun2[] {(x, y) => 2 * (Sin(y + 0.5) - 1) * Cos(y + 0.5)}
-            };
-
-            var tau = fGrad.Evaluate2(xk);
-
-            f.SolveWithNewtonMethod(fGrad, xk.CreateCopy());
-            f.SolveWithSimpleIterationMethod(tau, xk.CreateCopy());
-            minimizedF.SolveWithGradientDescendMethod(minimizedGrad, parameters, xk.CreateCopy());
+            minimizedF.SolveWithGradientDescendMethod(minimizedGrad, parameters, values.CreateCopy());
         }
     }
-
-    public delegate double Fun2(double x, double y);
 
     public class GradientDescendParameters
     {
@@ -57,108 +60,106 @@ namespace NonLinearEquations
 
     public static class SimpleIterationMethod
     {
-        public static void SolveWithSimpleIterationMethod(this Fun2[][] f, double[][] tau, double[][] xk)
+        public static void SolveWithSimpleIterationMethod(this ITerm[][] f, ITerm[][] fGrad,
+            double[][] values)
         {
-            Console.WriteLine("{0,5}|{1,15}|{2,15}|{3,15}|{4,15}|{5,15}",
-                "i", "x", "y", "f1", "f2", "residual");
-
-            for (var i = 1; i <= 100; i++)
-            {
-                var fValues = f.Evaluate2(xk);
-
-                xk.Add(tau.Multiply(fValues));
-
-                fValues = f.Evaluate2(xk);
-                var residual = fValues.Norm3();
-
-                Console.WriteLine("{0,5}|{1,15:G6}|{2,15:G6}|{3,15:G6}|{4,15:G6}|{5,15:G6}",
-                    i, xk[0][0], xk[1][0], fValues[0][0], fValues[1][0], residual);
-            }
-        }
-    }
-
-    public static class NewtonMethod
-    {
-        public static void SolveWithNewtonMethod(this Fun2[][] f, Fun2[][] fGrad, double[][] xk)
-        {
-            Console.WriteLine(fGrad.Evaluate2(xk).Norm3());
+            var tau = fGrad.Evaluate(values);
+            Console.WriteLine("tau:");
+            Console.WriteLine(tau.ToPrettyString());
+            Console.WriteLine($"Norm={tau.Norm3()}");
 
             Console.WriteLine("{0,5}|{1,15}|{2,15}|{3,15}|{4,15}|{5,15}",
                 "i", "x", "y", "f1", "f2", "residual");
 
             for (var i = 1; i <= 10; i++)
             {
-                var fValues = f.Evaluate2(xk);
-                var gradValues = fGrad.Evaluate2(xk);
-                var inverseGradValues = gradValues.CreateInverse();
+                var fValues = f.Evaluate(values);
 
-                xk.Subtract(inverseGradValues.Multiply(fValues));
+                values.Add(tau.Multiply(fValues));
 
-                fValues = f.Evaluate2(xk);
+                fValues = f.Evaluate(values);
                 var residual = fValues.Norm3();
 
                 Console.WriteLine("{0,5}|{1,15:G6}|{2,15:G6}|{3,15:G6}|{4,15:G6}|{5,15:G6}",
-                    i, xk[0][0], xk[1][0], fValues[0][0], fValues[1][0], residual);
+                    i, values[0][0], values[1][0], fValues[0][0], fValues[1][0], residual);
+            }
+        }
+    }
+
+    public static class NewtonMethod
+    {
+        public static void SolveWithNewtonMethod(this ITerm[][] f, ITerm[][] fGrad, double[][] values)
+        {
+            Console.WriteLine("{0,5}|{1,15}|{2,15}|{3,15}|{4,15}|{5,15}",
+                "i", "x", "y", "f1", "f2", "residual");
+
+            for (var i = 1; i <= 10; i++)
+            {
+                var fValues = f.Evaluate(values);
+                var gradValues = fGrad.Evaluate(values);
+                var inverseGradValues = gradValues.CreateInverse();
+
+                values.Subtract(inverseGradValues.Multiply(fValues));
+
+                fValues = f.Evaluate(values);
+                var residual = fValues.Norm3();
+
+                Console.WriteLine("{0,5}|{1,15:G6}|{2,15:G6}|{3,15:G6}|{4,15:G6}|{5,15:G6}",
+                    i, values[0][0], values[1][0], fValues[0][0], fValues[1][0], residual);
             }
         }
     }
 
     public static class GradientDescendMethod
     {
-        public static void SolveWithGradientDescendMethod(this Fun2 f, Fun2[][] fGrad, GradientDescendParameters parameters,
-            double[][] xk)
+        public static void SolveWithGradientDescendMethod(this ITerm f, ITerm[][] fGrad,
+            GradientDescendParameters parameters, double[][] values)
         {
-            Console.WriteLine(fGrad.Evaluate2(xk).Norm3());
+            Console.WriteLine(fGrad.Evaluate(values).Norm3());
 
             Console.WriteLine("{0,5}|{1,15}|{2,15}|{3,15}|{4,15}",
                 "i", "x", "y", "F", "alpha");
 
             for (var i = 1; i <= 100; i++)
             {
-                var gradValues = fGrad.Evaluate2(xk);
+                var gradValues = fGrad.Evaluate(values);
                 gradValues.Multiply(parameters.Alpha);
-                var arg = xk.CreateCopy();
+                var arg = values.CreateCopy();
                 arg.Subtract(gradValues);
 
-                while (f.Evaluate2(arg) >= f.Evaluate2(xk))
+                while (f.Evaluate(arg) >= f.Evaluate(values))
                 {
                     parameters.Alpha *= parameters.Lambda;
 
-                    gradValues = fGrad.Evaluate2(xk);
+                    gradValues = fGrad.Evaluate(values);
                     gradValues.Multiply(parameters.Alpha);
-                    arg = xk.CreateCopy();
+                    arg = values.CreateCopy();
                     arg.Subtract(gradValues);
                 }
 
-                xk = arg;
-                var value = f.Evaluate2(xk);
+                values = arg;
+                var value = f.Evaluate(values);
 
                 Console.WriteLine("{0,5}|{1,15:G6}|{2,15:G6}|{3,15:G6}|{4,15:G6}",
-                    i, xk[0][0], xk[1][0], value, parameters.Alpha);
+                    i, values[0][0], values[1][0], value, parameters.Alpha);
             }
         }
     }
 
     public static class Evaluation
     {
-        public static double[][] Evaluate2(this Fun2[][] matrix, double[][] values)
+        public static double[][] Evaluate(this ITerm[][] matrix, double[][] values)
         {
-            var copy = matrix.CreateCompatible<Fun2, double>();
+            var copy = matrix.CreateCompatible<ITerm, double>();
             for (var row = 0; row < matrix.Rows(); row++)
             {
                 for (var column = 0; column < matrix.Columns(); column++)
                 {
-                    copy[row][column] = matrix[row][column].Evaluate2(values);
+                    copy[row][column] = matrix[row][column].Evaluate(values);
                 }
             }
 
             return copy;
-        }
-
-        public static double Evaluate2(this Fun2 f, double[][] values)
-        {
-            var (x, y) = (values[0][0], values[1][0]);
-            return f(x, y);
         }
     }
 
@@ -191,15 +192,11 @@ namespace NonLinearEquations
             var pa = a.CreateCopy();
             pa.ApplySwaps(p);
 
-            // Check that LU = PA
-            var lu = l.Multiply(u);
-            lu.Subtract(pa);
-
             // Find A^(-1)
             var pe = pa.CreateOneMatrix();
             pe.ApplySwaps(p);
-            var y2 = l.FindY(pe);
-            var invertedA = u.FindX(y2);
+            var y = l.FindY(pe);
+            var invertedA = u.FindX(y);
             return invertedA;
         }
 
@@ -213,40 +210,16 @@ namespace NonLinearEquations
                 }
             }
         }
-    }
 
-    public static class MatrixComparison
-    {
-        public static bool GreaterOrEqual(this double[][] matrix, double[][] other)
+        public static void Map(this double[][] matrix, Func<double, double> f)
         {
             for (var i = 0; i < matrix.Rows(); i++)
             {
                 for (var j = 0; j < matrix.Columns(); j++)
                 {
-                    if (matrix[i][j] < other[i][j])
-                    {
-                        return false;
-                    }
+                    matrix[i][j] = f(matrix[i][j]);
                 }
             }
-
-            return true;
-        }
-
-        public static bool CloserThan(this double[][] matrix, double[][] other)
-        {
-            for (var i = 0; i < matrix.Rows(); i++)
-            {
-                for (var j = 0; j < matrix.Columns(); j++)
-                {
-                    if (Abs(matrix[i][j]) > Abs(other[i][j]))
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
         }
     }
 }
