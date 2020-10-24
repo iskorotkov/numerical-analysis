5802cd9fb64f3fefe347865cda7b5c5b97e90e9e
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-24 21:57:01 +0500

    refactor(equations): extract methods and add comments to them

--- src/NonLinearEquations/Program.cs
+++ src/NonLinearEquations/Program.cs
@@ -61,7 +61,7 @@ namespace NonLinearEquations
             //};
 
             // Parameters for gradient descend method
-            var parameters = new GradientDescendParameters
+            var gdParams = new GradientDescendSolver.Params
             {
                 Alpha = 1.0,
                 Lambda = 0.5
@@ -105,17 +105,16 @@ namespace NonLinearEquations
             // Prepare output file
             using var outputFile = File.Create(outputFileName);
             using var writer = new StreamWriter(outputFile);
-            PrintHeader(writer, values, parameters);
+            PrintHeader(writer, values, gdParams);
 
             // Calculations
             new SimpleIterationSolver(writer, epsilon).Solve(f, fi, fiGrad, values.CreateCopy());
             new NewtonSolver(writer, epsilon).Solve(f, fGrad, values.CreateCopy());
-            new GradientDescendSolver(writer, epsilon, parameters).Solve(minF, minFGrad, f, values.CreateCopy());
+            new GradientDescendSolver(writer, epsilon, gdParams).Solve(minF, minFGrad, f, values.CreateCopy());
         }
 
         // Print header with values of x and parameters for gradient descend method
-        private static void PrintHeader(TextWriter writer, double[][] values,
-            GradientDescendParameters parameters)
+        private static void PrintHeader(TextWriter writer, double[][] values, GradientDescendSolver.Params parameters)
         {
             writer.WriteLine($"x:\n{values.ToPrettyString()}");
             writer.WriteLine($"\nParameters:\n\talpha = {parameters.Alpha}\n\tlambda = {parameters.Lambda}");
@@ -138,14 +137,12 @@ namespace NonLinearEquations
         // Solve system f with provided approximate values
         public void Solve(ITerm[][] f, ITerm[][] fi, ITerm[][] fiGrad, double[][] values)
         {
-            _streamWriter.WriteLine("\n=== Simple iteration method ===");
+            WriteMethodName();
 
             var jakobiValues = fiGrad.Evaluate(values);
-            _streamWriter.WriteLine("\nJakobi:");
-            _streamWriter.WriteLine(jakobiValues.ToPrettyString());
-
             var jakobiNorm = jakobiValues.Norm3();
-            _streamWriter.WriteLine($"\nJakobi norm = {jakobiNorm}\n");
+
+            WriteInfo(jakobiValues, jakobiNorm);
 
             if (jakobiNorm >= 1d)
             {
@@ -153,8 +150,13 @@ namespace NonLinearEquations
                 return;
             }
 
-            _streamWriter.WriteLine($"{"i",3}{"x",15}{"y",15}{"residual norm",15}{"f1",15}{"f2",15}{"jakobi norm",15}");
+            WriteTableHeader();
+            IterateUntilConverges(f, fi, fiGrad, values);
+        }
 
+        // Make iteration until method converges with given epsilon
+        private void IterateUntilConverges(ITerm[][] f, ITerm[][] fi, ITerm[][] fiGrad, double[][] values)
+        {
             var i = 0;
             double[][] xDiff;
             do
@@ -164,17 +166,41 @@ namespace NonLinearEquations
                 var previousValues = values;
                 values = fi.Evaluate(values);
 
-                var fValues = f.Evaluate(values);
-                var residualNorm = fValues.Norm3();
-                jakobiValues = fiGrad.Evaluate(values);
-                jakobiNorm = jakobiValues.Norm3();
-
-                _streamWriter.WriteLine(
-                    $"{i,3}{values[0][0],15:G6}{values[1][0],15:G6}{residualNorm,15:G6}{fValues[0][0],15:G6}{fValues[1][0],15:G6}{jakobiNorm,15:G6}");
+                WriteIterationInfo(i, f, fiGrad, values);
 
                 xDiff = previousValues;
                 xDiff.Subtract(values);
             } while (!Converged(xDiff));
+        }
+
+        // Print info about iteration i
+        private void WriteIterationInfo(int i, ITerm[][] f, ITerm[][] fiGrad, double[][] values)
+        {
+            var fValues = f.Evaluate(values);
+            var residualNorm = fValues.Norm3();
+            var jakobiValues = fiGrad.Evaluate(values);
+            var jakobiNorm = jakobiValues.Norm3();
+
+            _streamWriter.WriteLine(
+                $"{i,3}{values[0][0],15:G6}{values[1][0],15:G6}{residualNorm,15:G6}{fValues[0][0],15:G6}{fValues[1][0],15:G6}{jakobiNorm,15:G6}");
+        }
+
+        private void WriteTableHeader()
+        {
+            _streamWriter.WriteLine($"{"i",3}{"x",15}{"y",15}{"residual norm",15}{"f1",15}{"f2",15}{"jakobi norm",15}");
+        }
+
+        // Print Jakobi norm value
+        private void WriteInfo(double[][] jakobiValues, double jakobiNorm)
+        {
+            _streamWriter.WriteLine("\nJakobi:");
+            _streamWriter.WriteLine(jakobiValues.ToPrettyString());
+            _streamWriter.WriteLine($"\nJakobi norm = {jakobiNorm}\n");
+        }
+
+        private void WriteMethodName()
+        {
+            _streamWriter.WriteLine("\n=== Simple iteration method ===");
         }
 
         // Check whether the method converged
@@ -182,16 +208,6 @@ namespace NonLinearEquations
             xDiff.All(row => row
                 .Select(Math.Abs)
                 .All(value => value < _eps));
-    }
-
-    // Set of parameters for gradient descend method
-    public class GradientDescendParameters
-    {
-        // Alpha controls speed of gradient descend
-        public double Alpha { get; set; } = 1.0;
-
-        // Lambda is a multiplier by which Alpha gets multiplier in order to reduce gradient descend speed
-        public double Lambda { get; set; } = 0.5;
     }
 
     // Solver based on Newton method
@@ -210,10 +226,14 @@ namespace NonLinearEquations
         // Solve system f with provided approximate values
         public void Solve(ITerm[][] f, ITerm[][] fGrad, double[][] values)
         {
-            _streamWriter.WriteLine("\n=== Newton method ===\n");
+            WriteMethodName();
+            WriteTableHeader();
+            IterateUntilConverges(f, fGrad, values);
+        }
 
-            _streamWriter.WriteLine($"{"i",3}{"x",15}{"y",15}{"residual norm",15}{"f1",15}{"f2",15}");
-
+        // Make iteration until method converges with given epsilon
+        private void IterateUntilConverges(ITerm[][] f, ITerm[][] fGrad, double[][] values)
+        {
             var i = 0;
             var fValues = f.Evaluate(values);
             double[][] previousFValues;
@@ -221,17 +241,40 @@ namespace NonLinearEquations
             {
                 i++;
 
-                var gradValues = fGrad.Evaluate(values);
-                var inverseGradValues = gradValues.CreateInverse();
-                values.Subtract(inverseGradValues.Multiply(fValues));
+                RecalculateX(fGrad, values, fValues);
 
                 previousFValues = fValues;
                 fValues = f.Evaluate(values);
-                var residual = fValues.Norm3();
 
-                _streamWriter.WriteLine(
-                    $"{i,3}{values[0][0],15:G6}{values[1][0],15:G6}{residual,15:G6}{fValues[0][0],15:G6}{fValues[1][0],15:G6}");
+                WriteIterationInfo(i, values, fValues);
             } while (!Converged(previousFValues));
+        }
+
+        // Recalculate x = x - f(x)/f'(x)
+        private static void RecalculateX(ITerm[][] fGrad, double[][] x, double[][] fValues)
+        {
+            var gradValues = fGrad.Evaluate(x);
+            var inverseGradValues = gradValues.CreateInverse();
+            x.Subtract(inverseGradValues.Multiply(fValues));
+        }
+
+        // Write info about iteration i
+        private void WriteIterationInfo(int i, double[][] values, double[][] fValues)
+        {
+            var residual = fValues.Norm3();
+
+            _streamWriter.WriteLine(
+                $"{i,3}{values[0][0],15:G6}{values[1][0],15:G6}{residual,15:G6}{fValues[0][0],15:G6}{fValues[1][0],15:G6}");
+        }
+
+        private void WriteTableHeader()
+        {
+            _streamWriter.WriteLine($"{"i",3}{"x",15}{"y",15}{"residual norm",15}{"f1",15}{"f2",15}");
+        }
+
+        private void WriteMethodName()
+        {
+            _streamWriter.WriteLine("\n=== Newton method ===\n");
         }
 
         // Check whether the method converged
@@ -244,12 +287,22 @@ namespace NonLinearEquations
     // Solver based on gradient descend method
     public class GradientDescendSolver
     {
+        // Set of parameters for gradient descend method
+        public class Params
+        {
+            // Alpha controls speed of gradient descend
+            public double Alpha { get; set; } = 1.0;
+
+            // Lambda is a multiplier by which Alpha gets multiplier in order to reduce gradient descend speed
+            public double Lambda { get; set; } = 0.5;
+        }
+
         private readonly StreamWriter _streamWriter;
         private readonly double _eps;
-        private readonly GradientDescendParameters _parameters;
+        private readonly Params _parameters;
 
         // Create solver that sends output to streamWriter with given epsilon and provided parameters
-        public GradientDescendSolver(StreamWriter streamWriter, double epsilon, GradientDescendParameters parameters)
+        public GradientDescendSolver(StreamWriter streamWriter, double epsilon, Params parameters)
         {
             _streamWriter = streamWriter;
             _eps = epsilon;
@@ -259,14 +312,15 @@ namespace NonLinearEquations
         // Minimize function minF with provided approximate values
         public void Solve(ITerm minF, ITerm[][] minFGrad, ITerm[][] f, double[][] values)
         {
-            _streamWriter.WriteLine("\n=== Gradient descend method ===");
+            WriteMethodName();
+            WriteInfo(minFGrad, values);
+            WriteTableHeader();
+            IterateUntilConverges(minF, minFGrad, f, values);
+        }
 
-            var gradient = minFGrad.Evaluate(values);
-            _streamWriter.WriteLine($"\nGradient vector:\n{gradient.ToPrettyString()}\n");
-
-            _streamWriter.WriteLine(
-                $"{"i",3}{"x",15}{"y",15}{"alpha",15}{"residual",15}{"f1",15}{"f2",15}{"F",15}{"k",3}");
-
+        // Make iteration until method converges with given epsilon
+        private void IterateUntilConverges(ITerm minF, ITerm[][] minFGrad, ITerm[][] f, double[][] values)
+        {
             var k = 0;
             var i = 0;
             double[][] xDiff;
@@ -275,7 +329,6 @@ namespace NonLinearEquations
                 i++;
 
                 var newValues = RecalculateValues(minFGrad, values);
-
                 while (minF.Evaluate(newValues) >= minF.Evaluate(values))
                 {
                     k++;
@@ -286,16 +339,40 @@ namespace NonLinearEquations
                 var previousX = values;
                 values = newValues;
 
-                var fValues = f.Evaluate(values);
-                var residual = fValues.Norm3();
-                var minFValue = minF.Evaluate(values);
-
-                _streamWriter.WriteLine(
-                    $"{i,3}{values[0][0],15:G6}{values[1][0],15:G6}{_parameters.Alpha,15:G6}{residual,15:G6}{fValues[0][0],15:G6}{fValues[1][0],15:G6}{minFValue,15:G6}{k,3}");
+                WriteIterationInfo(i, minF, f, values, k);
 
                 xDiff = previousX;
                 xDiff.Subtract(values);
             } while (!Converged(xDiff));
+        }
+
+        // Write info about iteration i
+        private void WriteIterationInfo(int i, ITerm minF, ITerm[][] f, double[][] values, int k)
+        {
+            var fValues = f.Evaluate(values);
+            var residual = fValues.Norm3();
+            var minFValue = minF.Evaluate(values);
+
+            _streamWriter.WriteLine(
+                $"{i,3}{values[0][0],15:G6}{values[1][0],15:G6}{_parameters.Alpha,15:G6}{residual,15:G6}{fValues[0][0],15:G6}{fValues[1][0],15:G6}{minFValue,15:G6}{k,3}");
+        }
+
+        private void WriteTableHeader()
+        {
+            _streamWriter.WriteLine(
+                $"{"i",3}{"x",15}{"y",15}{"alpha",15}{"residual",15}{"f1",15}{"f2",15}{"F",15}{"k",3}");
+        }
+
+        // Write value of gradient vector
+        private void WriteInfo(ITerm[][] minFGrad, double[][] values)
+        {
+            var gradient = minFGrad.Evaluate(values);
+            _streamWriter.WriteLine($"\nGradient vector:\n{gradient.ToPrettyString()}\n");
+        }
+
+        private void WriteMethodName()
+        {
+            _streamWriter.WriteLine("\n=== Gradient descend method ===");
         }
 
         // Recalculate value of matrix x
@@ -342,11 +419,37 @@ namespace NonLinearEquations
             var u = a.CreateCopy();
             var p = a.CreateSwapMatrix();
 
-            // Make LU decomposition
+            MakeLupDecomposition(u, l, p);
+            return FindInverseMatrix(p, a, l, u);
+        }
+
+        // Find inverse matrix using LU = PA
+        private static double[][] FindInverseMatrix(int[] p, double[][] a, double[][] l, double[][] u)
+        {
+            var pa = a.CreateCopy();
+            pa.ApplySwaps(p);
+
+            var pe = pa.CreateOneMatrix();
+            pe.ApplySwaps(p);
+
+            return SolveEquation(l, u, pe);
+        }
+
+        // Solve equation AX = LUX = E and find X = A^(-1)
+        private static double[][] SolveEquation(double[][] l, double[][] u, double[][] pe)
+        {
+            var y = l.FindY(pe);
+            var invertedA = u.FindX(y);
+            return invertedA;
+        }
+
+        // Make LUP decomposition for given matrix a (LU = PA)
+        private static void MakeLupDecomposition(double[][] a, double[][] l, int[] p)
+        {
+            var u = a;
             for (var row = 0; row < u.Rows(); row++)
             {
                 var rowToSwap = u.RowToSwapWith(row);
-
                 if (rowToSwap != row)
                 {
                     u.SwapRows(row, rowToSwap);
@@ -359,16 +462,6 @@ namespace NonLinearEquations
                 u.SubtractRow(row);
                 u.NormalizeRow(row);
             }
-
-            var pa = a.CreateCopy();
-            pa.ApplySwaps(p);
-
-            // Find A^(-1)
-            var pe = pa.CreateOneMatrix();
-            pe.ApplySwaps(p);
-            var y = l.FindY(pe);
-            var invertedA = u.FindX(y);
-            return invertedA;
         }
 
         // Multiply matrix by value
